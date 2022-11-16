00c241eb9dc7bc19591156fc8d66ea68527a63af
gau94rav <gau94rav@gmail.com>
2022-11-16 19:12:35 +0530

    prettier config

--- src/components/EditorComponent.vue
+++ src/components/EditorComponent.vue
@@ -25,55 +25,55 @@
 </template>
 
 <script setup>
-import * as components from "../utils/components";
-import { Icon } from "@iconify/vue";
-import { useEditorStore } from "../stores/editor";
-import { ref } from "vue";
+import * as components from '../utils/components'
+import { Icon } from '@iconify/vue'
+import { useEditorStore } from '../stores/editor'
+import { ref } from 'vue'
 
-const editor = useEditorStore();
-const showEditIconIndex = ref(false);
+const editor = useEditorStore()
+const showEditIconIndex = ref(false)
 
 function handleDragStart(component) {
-  editor.setIsDragging(true);
-  editor.setCanBeDeleted(true);
-  editor.setSelectedComponent(component);
+  editor.setIsDragging(true)
+  editor.setCanBeDeleted(true)
+  editor.setSelectedComponent(component)
 }
 
 function handleDrop() {
-  editor.setIsDragging(false);
-  editor.setCanBeDeleted(false);
-  editor.setSelectedComponent({});
+  editor.setIsDragging(false)
+  editor.setCanBeDeleted(false)
+  editor.setSelectedComponent({})
 }
 
 function handleDragOver(index) {
   // Components before hovered index
   const beforeComponent = editor.componentTree
     .slice(0, index)
-    .filter((c) => c.id !== editor.selectedComponent.id);
+    .filter((c) => c.id !== editor.selectedComponent.id)
 
   // Components after hovered index
   const afterComponent = editor.componentTree
     .slice(index)
-    .filter((c) => c.id !== editor.selectedComponent.id);
+    .filter((c) => c.id !== editor.selectedComponent.id)
 
   // Append selected component in between before and after
   editor.setComponentTree([
     ...beforeComponent,
     editor.selectedComponent,
     ...afterComponent,
-  ]);
+  ])
 }
 
 function handleDragLeave(index) {
-  const lastIndex = editor.componentTree.length - 1;
-  let components = [];
+  const lastIndex = editor.componentTree.length - 1
+  let components = []
   if (lastIndex === index) {
     const withoutSelected = editor.componentTree.filter(
       (c) => c.id !== editor.selectedComponent.id
-    );
-    console.log(withoutSelected);
-    components = [...withoutSelected, editor.selectedComponent];
-    editor.setComponentTree(components);
+    )
+    console.log(withoutSelected)
+    components = [...withoutSelected, editor.selectedComponent]
+    editor.setComponentTree(components)
   }
 }
 </script>
